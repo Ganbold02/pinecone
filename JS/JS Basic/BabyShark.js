a3f7d1cc4ca538e694fd99969a5496c305c125e3
ganbold02 <u.boldoo@gmail.com>
2022-11-28 17:24:12 +0800

    kj

---
+++ JS/JS Basic/BabyShark.js
@@ -0,0 +1,46 @@
+let baby = "Baby Shark",
+  doo = "doo-doo",
+  mommy = "Mommy Shark",
+  daddy = "Daddy Shark",
+  Grandma = "Grandma Shark",
+  Grandpa = "Grandpa Shark",
+  LetGo = "Let's go hunt",
+  RunAway = "Run away",
+  SafeAt = "Safe at last,",
+  ItEnd = "It'sthe end";
+console.log(baby + ", " + doo + ", " + doo);
+console.log(baby + ", " + doo + ", " + doo);
+console.log(baby + ", " + doo + ", " + doo);
+console.log(baby);
+console.log(mommy + ", " + doo + ", " + doo);
+console.log(mommy + ", " + doo + ", " + doo);
+console.log(mommy + ", " + doo + ", " + doo);
+console.log(mommy);
+console.log(daddy + ", " + doo + ", " + doo);
+console.log(daddy + ", " + doo + ", " + doo);
+console.log(daddy + ", " + doo + ", " + doo);
+console.log(daddy);
+console.log(Grandma + ", " + doo + ", " + doo);
+console.log(Grandma + ", " + doo + ", " + doo);
+console.log(Grandma + "," + doo + ", " + doo);
+console.log(Grandma);
+console.log(Grandpa + ", " + doo + ", " + doo);
+console.log(Grandpa + ", " + doo + ", " + doo);
+console.log(Grandpa + ", " + doo + ", " + doo);
+console.log(Grandpa);
+console.log(LetGo + ", " + doo + ", " + doo);
+console.log(LetGo + ", " + doo + ", " + doo);
+console.log(LetGo + ", " + doo + ", " + doo);
+console.log(LetGo);
+console.log(RunAway + ", " + doo + ", " + doo);
+console.log(RunAway + ", " + doo + ", " + doo);
+console.log(RunAway + ", " + doo + ", " + doo);
+console.log(RunAway);
+console.log(SafeAt + ", " + doo + ", " + doo);
+console.log(SafeAt + ", " + doo + ", " + doo);
+console.log(SafeAt + ", " + doo + ", " + doo);
+console.log(SafeAt);
+console.log(ItEnd + ", " + doo + ", " + doo);
+console.log(ItEnd + ", " + doo + ", " + doo);
+console.log(ItEnd + ", " + doo + ", " + doo);
+console.log(ItEnd);
